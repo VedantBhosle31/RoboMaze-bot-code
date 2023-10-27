7ff32e8deab7bb6d1c8063ebf8ababf845eac846
QuantumRuler <bvedant8@gmail.com>
2023-10-27 18:37:40 +0530

    Changed Motor Comands

--- MotorControl/MotorControl.ino
+++ MotorControl/MotorControl.ino
@@ -12,98 +12,149 @@
 #define PIN_11   (11)
 #define PIN_12   (12)
 
-
-// Motor A connections
-int enA = 9;
-int in1 = 8;
-int in2 = 7;
-// Motor B connections
-int enB = 3;
-int in3 = 5;
-int in4 = 4;
+typedef struct Motor{
+  int in1;
+  int in2;
+  int en;
+};
+Motor right={8,7,9};
+Motor left={5,4,3};
+void MotorSetup(Motor motor){
+  pinMode(motor.en, OUTPUT);
+  pinMode(motor.in1, OUTPUT);
+  pinMode(motor.in2, OUTPUT);
+  digitalWrite(motor.in1, LOW);
+  digitalWrite(motor.in2, LOW);
+}
 
 void setup() {
 	// Set all the motor control pins to outputs
-	pinMode(enA, OUTPUT);
-	pinMode(enB, OUTPUT);
-	pinMode(in1, OUTPUT);
-	pinMode(in2, OUTPUT);
-	pinMode(in3, OUTPUT);
-	pinMode(in4, OUTPUT);
-	
-	// Turn off motors - Initial state
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, LOW);
-	digitalWrite(in3, LOW);
-	digitalWrite(in4, LOW);
+  MotorSetup(right);
+  MotorSetup(left);
 }
 
+void Turn(bool direction){
+  analogWrite(right.en, 255);
+	analogWrite(left.en, 255);
+  	if(direction){
+    digitalWrite(right.in1, LOW);
+    digitalWrite(right.in2, HIGH);
+    digitalWrite(left.in1, HIGH);
+    digitalWrite(left.in2, LOW);
+  }else{	
+    digitalWrite(right.in1, LOW);
+	  digitalWrite(right.in2, HIGH);
+	  digitalWrite(left.in1, LOW);
+    digitalWrite(left.in2, HIGH);
+  }
+}
 
+void forward(bool direction){
+  analogWrite(right.en, 255);
+	analogWrite(left.en, 255);
+
+  	if(direction){
+    digitalWrite(right.in1, LOW);
+    digitalWrite(right.in2, HIGH);
+    digitalWrite(left.in1, LOW);
+    digitalWrite(left.in2, HIGH);
+
+  }else{	
+    digitalWrite(right.in1, HIGH);
+	  digitalWrite(right.in2, LOW);
+	  digitalWrite(left.in1, HIGH);
+    digitalWrite(left.in2, LOW);
+
+  }
+
+}
+
+void stop(){
+  analogWrite(right.en, 0);
+	analogWrite(left.en, 0);
+
+  	// Now turn off motors
+	digitalWrite(right.in1, LOW);
+	digitalWrite(right.in2, LOW);
+	digitalWrite(left.in1, LOW);
+	digitalWrite(left.in2, LOW);
+
+}
 
 // This function lets you control spinning direction of motors
 void directionControl(bool direction) {
 	// Set motors to maximum speed
 	// For PWM maximum possible values are 0 to 255
-	analogWrite(enA, 255);
-	analogWrite(enB, 255);
+	analogWrite(right.en, 255);
+	analogWrite(left.en, 255);
 
 	// Turn on motor A & B
 
 	if(direction){
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
+    digitalWrite(right.in1, LOW);
+    digitalWrite(right.in2, HIGH);
+    digitalWrite(left.in1, HIGH);
+    digitalWrite(left.in2, LOW);
     delay(2000);
 
   }else{	
-    digitalWrite(in1, LOW);
-	  digitalWrite(in2, HIGH);
-	  digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
+    digitalWrite(right.in1, LOW);
+	  digitalWrite(right.in2, HIGH);
+	  digitalWrite(left.in1, LOW);
+    digitalWrite(left.in2, HIGH);
     delay(2000);
-
   }
 
 	// Turn off motors
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, LOW);
-	digitalWrite(in3, LOW);
-	digitalWrite(in4, LOW);
+	digitalWrite(right.in1, LOW);
+	digitalWrite(right.in2, LOW);
+	digitalWrite(left.in1, LOW);
+	digitalWrite(left.in2, LOW);
 }
 
 // This function lets you control speed of the motors
-void speedControl() {
+void speedControl(bool direction) {
+
 	// Turn on motors
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, HIGH);
-	digitalWrite(in3, LOW);
-	digitalWrite(in4, HIGH);
+	if(direction){
+    digitalWrite(right.in1, LOW);
+    digitalWrite(right.in2, HIGH);
+    digitalWrite(left.in1, LOW);
+    digitalWrite(left.in2, HIGH);
+    delay(2000);
+
+  }else{	
+    digitalWrite(right.in1, HIGH);
+	  digitalWrite(right.in2, LOW);
+	  digitalWrite(left.in1, HIGH);
+    digitalWrite(left.in2, LOW);
+    delay(2000);
+  }
+
 	
 	// Accelerate from zero to maximum speed
 	for (int i = 0; i < 256; i++) {
-		analogWrite(enA, i);
-		analogWrite(enB, i);
+		analogWrite(right.en, i);
+		analogWrite(left.en, i);
 		delay(20);
 	}
 	
 	// Decelerate from maximum speed to zero
 	for (int i = 255; i >= 0; --i) {
-		analogWrite(enA, i);
-		analogWrite(enB, i);
+		analogWrite(right.en, i);
+		analogWrite(left.en, i);
 		delay(20);
 	}
 	
 	// Now turn off motors
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, LOW);
-	digitalWrite(in3, LOW);
-	digitalWrite(in4, LOW);
+	digitalWrite(right.in1, LOW);
+	digitalWrite(right.in2, LOW);
+	digitalWrite(left.in1, LOW);
+	digitalWrite(left.in2, LOW);
 }
 
 void loop() {
-	directionControl(true);
-	delay(1000);
-	speedControl();
-	delay(1000);
+  forward(true);
+
+
 }
